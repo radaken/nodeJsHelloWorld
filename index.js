d783e947a1ba3e858943b43c644c222cba32a69b
Kulikov Alexey <alexon249@gmail.com>
2019-07-20 22:56:03 +0300

    Удаление & Создание файла и папки

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var fs = require('fs');
+
+//Удаление файлов & папки
+
+fs.unlink('./new-one/some_new.txt', function () {
+  fs.rmdir('new-one', function () { console.log("Srabotalo!"); });
+});
+
+//Создание папки & файлов
+
+// fs.mkdir('new-one', function() {
+//   fs.writeFile('./new-one/some_new.txt', 'Hello world!', function() {
+//     console.log("Всё сработало!");
+//   })
+// })
