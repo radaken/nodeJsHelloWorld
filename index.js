b9eaff2f2534332c4e1f1d3e8a2f933caba9f15d
Kulikov Alexey <alexon249@gmail.com>
2019-07-20 21:53:11 +0300

    Подключил встроенную библиотеку,  прочитал txt-файл и создал

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+var fs = require('fs');
+
+// Читаем файл
+var file_readed = fs.readFileSync('text.txt', 'utf8');
+
+// Создаём / редактируем файл
+var message = "Привет мир! \n" + file_readed;
+fs.writeFileSync('someNewFile.txt', message);
