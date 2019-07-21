4653d07309e7075c2437fbaaaa251e5b981bed62
Kulikov Alexey <alexon249@gmail.com>
2019-07-22 00:16:17 +0300

    yep! Did it!

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
   console.log("URL страницы:" + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  res.end('Привет мир!');
+  if ((req.url === '/index') || (req.url === '/') || (req.url === '/style.css')) {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  fs.createReadStream(__dirname + '/index.html').pipe(res);
+} else if (req.url === '/about') {
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  fs.createReadStream(__dirname + '/about.html').pipe(res);
+} else {
+  res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+  fs.createReadStream(__dirname + '/404.html').pipe(res);
+}
 })
 
 server.listen(3000, '127.0.0.1');
