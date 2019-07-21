e1164aa4b071d71a998d319ea5d1d3bfcbc5e9be
Kulikov Alexey <alexon249@gmail.com>
2019-07-21 22:54:54 +0300

    Дело сделано!
    скопировал с помощью потоков big.txt > news.txt.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+var fs = require('fs');
+
+var myReadShort = fs.createReadStream(__dirname + '/big.txt');
+var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
+
+myReadShort.on('data', function(chunk) {
+  console.log("Hoвые данные получены:");
+  myWriteShort.write(chunk);
+})
