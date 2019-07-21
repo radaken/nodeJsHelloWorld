bbf876a2f2c51dd18cf3096b15cf30f0f26c1164
Kulikov Alexey <alexon249@gmail.com>
2019-07-21 22:37:31 +0300

    Создал первый сервер NodeJS

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  console.log("URL страницы:" + req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+  res.end('Привет мир!');
+})
+
+server.listen(3000, '127.0.0.1');
+console.log("Мы отслеживаем порт 3000");
+//Заходить на 127.0.0.1:3000
