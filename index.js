0105b728af9f92ed846b031c58ffe4de5d80b568
Kulikov Alexey <alexon249@gmail.com>
2019-07-21 23:18:24 +0300

    Completed!

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var fs = require('fs');
+var http = require('http');
 
-var myReadShort = fs.createReadStream(__dirname + '/big.txt');
-var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
-
-myReadShort.on('data', function(chunk) {
-  console.log("Hoвые данные получены:");
-  myWriteShort.write(chunk);
+var server = http.createServer(function(req, res) {
+  console.log("URL страницы:" + req.url);
+/*Вывод HTML
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
+  myReadShort.pipe(res);
+  */
+  //Вывод JSON
+  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+  var obj = {
+    model: 'Audi',
+    speed: '234',
+    wheels: 4
+  };
+  res.end(JSON.stringify(obj));
 })
+
+
+server.listen(3000, '127.0.0.1');
+console.log("Мы отслеживаем порт 3000");
+//Заходить на 127.0.0.1:3000
